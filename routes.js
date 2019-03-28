c17060265e791e1fb2aac16dd2fc2a8188cc2a36
Gabriel Santiago <gisantiago@live.com>
2019-03-28 13:28:02 -0400

    Update routes.js

--- routes.js
+++ routes.js
@@ -7,22 +7,26 @@ const User = require("./models").User;
 const Course = require("./models").Course;
 
 
-
-
 router.param("_id", (req, res, next, id) => {
-    User.findById(id, (err, doc) => {
+    Course.findById(id, (err, doc) => {
         if(err) return next(err);
         if(!doc) {
             err = new Error("Not Found");
             err.status = 404;
             return next(err);
         }
-        req.user = doc;
+        req.course = doc;
         return next();
     });
 });
 
-//GET: /api/users --- All Users
+
+
+/*****************************************************************
+ *  USERS ROUTES
+****************************************************************/
+
+//GET: /api/users 200 --- All Users
 router.get("/users", (req, res, next) => {
     User.find({})
                 .exec((err, users) => {
@@ -31,9 +35,40 @@ router.get("/users", (req, res, next) => {
                 });
 });
 
-//GET:
+//POST: /api/users 201
+//Route for creating a user
+router.post("/users", (req, res, next) => {
+    const user = new User(req.body);
+    user.save( (err, user) => {
+        if(err) return next(err);
+        res.status(201);
+        res.json(user);
+    });
+});
 
-//POST: /courses
+
+/*****************************************************************
+ *  COURSES ROUTES
+****************************************************************/
+
+//GET: /api/courses 200 --- All Course
+router.get("/courses", (req, res, next) => {
+    Course.find({})
+                .exec((err, courses) => {
+                    if(err) return next(err);
+                    res.json(courses);
+                });
+});
+
+
+//GET: /api/courses/:id 201
+//Route that resturns a course based on the provided course ID
+router.get("/courses/:id", (req, res, next) => {
+     res.json(req.course);
+}); 
+
+
+//POST: /api/courses 201
 //Route for creating a course
 router.post("/courses", (req, res, next) => {
     const course = new Course(req.body);
@@ -45,25 +80,9 @@ router.post("/courses", (req, res, next) => {
 });
 
 
-//GET: /api/users --- All Course
-router.get("/courses", (req, res, next) => {
-    Course.find({})
-                .exec((err, courses) => {
-                    if(err) return next(err);
-                    res.json(courses);
-                });
-});
 
-//POST: /users
-//Route for creating a user
-router.post("/users", (req, res, next) => {
-    const user = new User(req.body);
-    user.save( (err, user) => {
-        if(err) return next(err);
-        res.status(201);
-        res.json(user);
-    });
-});
+
+
 
 
 module.exports = router;
